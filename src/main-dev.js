831b4f1f12f3188d47c01f36caed1ed52ae3ecd2
eight <641137800@qq.com>
2025-01-04 21:13:58 +0800

    update

--- src/main-dev.js
+++ src/main-dev.js
@@ -40,7 +40,7 @@ Vue.use(vueBaberrage)
 //导入axios
 import axios from 'axios'
 //配置请求的根路径
-axios.defaults.baseURL = 'http://localhost:8889/api/'
+axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL
 Vue.prototype.$http = axios
 // const CancelToken = axios.CancelToken;
 // const source = CancelToken.source();
